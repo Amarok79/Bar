5b7ed9a11644bbf0330f5330279f6d2ddf28ecf1
Olaf Kober <olaf.kober@outlook.com>
2019-09-17 21:59:22 +0200

    .

--- src/Drinks.Viewer/Viewer/BindableBase.cs
+++ src/Drinks.Viewer/Viewer/BindableBase.cs
@@ -1,4 +1,28 @@
-﻿//  ---------------------------------------------------------------------------------
+﻿/* MIT License
+ * 
+ * Copyright (c) 2019, Olaf Kober
+ * https://bitbucket.org/Amarok/bar
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+//  ---------------------------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All rights reserved.
 // 
 //  The MIT License (MIT)
@@ -22,6 +46,9 @@
 //  THE SOFTWARE.
 //  ---------------------------------------------------------------------------------
 
+#pragma warning disable S1694 // An abstract class should have both abstract and concrete methods
+#pragma warning disable S3874 // "out" and "ref" parameters should not be used
+
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -40,6 +67,14 @@ namespace Drinks.Viewer
 		/// Multicast event for property change notifications.
 		/// </summary>
 		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		/// <summary>
+		/// Initializes a new instance.
+		/// </summary>
+		protected BindableBase()
+		{
+		}
 
 		/// <summary>
 		/// Checks if a property already matches a desired value.  Sets the property and
