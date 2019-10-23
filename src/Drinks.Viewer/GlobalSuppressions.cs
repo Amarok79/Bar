0fd92d83bc307c9bdcaedc02eaf9628de0c7b862
Olaf Kober <olaf.kober@outlook.com>
2019-10-23 20:53:24 +0000

    .

--- src/Drinks.Viewer/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Minor Code Smell", "S3241:Methods should not return values that are never used", Justification = "<Pending>", Scope = "member", Target = "~M:Drinks.Viewer.MainPage.MainPage_obj1_Bindings.SetDataRoot(System.Object)~System.Boolean")]
