3bd00645a3b78dcaa6ce918ed242a1efbda47b21
Olaf Kober <olaf.kober@outlook.com>
2020-01-06 18:28:50 +0100

    added new Xamarin Forms mobile projects

---
+++ src/Drinks.Viewer.Mobile/Drinks.Viewer.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Drinks.Viewer.Mobile.Services;
+using Drinks.Viewer.Mobile.Views;
+
+namespace Drinks.Viewer.Mobile
+{
+	public partial class App : Application
+	{
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new AppShell();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ src/Drinks.Viewer.Mobile/Drinks.Viewer.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Drinks.Viewer.Mobile.Models;
+
+namespace Drinks.Viewer.Mobile.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
