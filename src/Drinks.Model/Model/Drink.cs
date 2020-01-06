9c4cde29c8d58ce2bfc115d1851d32958a12e1db
Olaf Kober <olaf.kober@outlook.com>
2020-01-06 18:51:22 +0100

    fixed file headers

--- src/Drinks.Model/Model/Drink.cs
+++ src/Drinks.Model/Model/Drink.cs
@@ -1,7 +1,7 @@
 /* MIT License
  * 
  * Copyright (c) 2019, Olaf Kober
- * https://bitbucket.org/Amarok/bar
+ * https://github.com/Amarok79/Bar
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
